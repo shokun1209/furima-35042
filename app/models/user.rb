d1000717a7c7f55b0526cb89bee2b99c8ca73c14
shokun1209 <shoya@satoushouyanoMacBook-Air.local>
2021-03-26 21:46:26 +0900

    新規登録,ログイン,ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        #  validates :email, unique: true
+         validates :nickname, presence: true
+         validates :first_name, presence: true
+         validates :last_name, presence: true
+         validates :first_name_kana, presence: true, format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+         validates :last_name_kana, presence: true, format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+         validates :birthday, presence: true
 end

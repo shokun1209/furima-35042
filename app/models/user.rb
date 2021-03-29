f86d6b7e3637ef52ae5e3ab653b885a056459e6e
shokun1209 <shoya@satoushouyanoMacBook-Air.local>
2021-03-29 16:52:07 +0900

    userモデルの単体テストコード記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
         #  validates :email, unique: true
          validates :nickname, presence: true
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :first_name_kana, presence: true, format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/}
-         validates :last_name_kana, presence: true, format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+         validates :first_name, presence: true, format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} #平仮名、カタカナ、漢字のみ
+         validates :last_name, presence: true, format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} #平仮名、カタカナ、漢字のみ
+         validates :first_name_kana, presence: true, format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/} #カタカナのみ
+         validates :last_name_kana, presence: true, format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/} #カタカナのみ
          validates :birthday, presence: true
+
 end

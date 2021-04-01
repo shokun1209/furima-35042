2e3d9c625fd2909644446f645d783ebe52c2aa1a
shokun1209 <shoya@satoushouyanoMacBook-Air.local>
2021-04-01 19:32:09 +0900

    editアクション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless current_user.id == @item.user.id
+    unless user_signed_in? && current_user.id == @item.user.id
       redirect_to root_path
     end
   end

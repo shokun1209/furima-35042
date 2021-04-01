e40ba6539ecd7542495331fc784f59fffdb28786
shokun1209 <shoya@satoushouyanoMacBook-Air.local>
2021-04-01 21:39:26 +0900

    スペルミス修正　redirect設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:show, :edit, :update, :destory]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :set_validates, only: [:edit, :update]
 
   def index
@@ -14,7 +14,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to action: :index
+      redirect_to root_path
     else
       render :new
     end
@@ -34,7 +34,10 @@ class ItemsController < ApplicationController
     end
   end
 
-  def destory
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    end
   end
 
   private

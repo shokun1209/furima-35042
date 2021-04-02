03309d74cb1bf896adb71429b6b7eccfa96387b2
shokun1209 <shoya@satoushouyanoMacBook-Air.local>
2021-04-02 21:34:53 +0900

    マイグレーション記述

--- db/migrate/20210402122402_create_addresses.rb
+++ db/migrate/20210402122402_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.integer :area_id, null: false
+      t.string :municipality, null: false
+      t.string :address, null: false
+      t.string :building
+      t.string :postal_code, null: false
+      t.string :tell, null: false
+      t.references :purchase_record, null: false, foreign_key: true
       t.timestamps
     end
   end

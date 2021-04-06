7ecfddeb8b879bb71073826dae81f34556c3bb62
shokun1209 <shoya@satoushouyanoMacBook-Air.local>
2021-04-06 20:04:10 +0900

    クレジット機能の実装

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,20 +1,33 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :set_item, only: [:index, :create]
   def index
     @purchase_record_address = PurchaseRecordAddress.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
     @purchase_record_address = PurchaseRecordAddress.new(purchase_records_params)
-    if @purchase_record_address.save
-      redirect_to root_path
+    if @purchase_record_address.valid?
+      set_pay
+      @purchase_record_address.save
+      return redirect_to root_path
     else
       render :index
     end
   end
 
   private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
   def purchase_records_params
-    params.require(:purchase_record_address).permit(:area_id, :municipality, :address, :building, :postal_code, :tell).merge(user_id: current_user.id, item_id: params[:item_id] )
+    params.require(:purchase_record_address).permit(:area_id, :municipality, :address, :building, :postal_code, :tell).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+  def set_pay
+    Payjp.api_key = "sk_test_d9a4a920f98c6cebeb4af6e0"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_records_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,6 @@
 const pay = () => {
-  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
-  const form = document.getElementClassName("charge-form");
+  Payjp.setPublicKey("pk_test_65e3477efae6dd2541a3a72b");
+  const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -8,10 +8,10 @@ const pay = () => {
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("purchase_record[number]"),
-      cvc: formData.get("purchase_record[cvc]"),
-      exp_month: formData.get("purchase_record[exp_month]"),
-      exp_year: `20${formData.get("purchase_record[exp_year]")}`,
+      number: formData.get("purchase_record_address[number]"),
+      cvc: formData.get("purchase_record_address[cvc]"),
+      exp_month: formData.get("purchase_record_address[exp_month]"),
+      exp_year: `20${formData.get("purchase_record_address[exp_year]")}`,
     };
 
     Payjp.createToken(card, (status, response) => {
@@ -21,10 +21,10 @@ const pay = () => {
         const tokenObj = `<input value=${token} name='token' type="hidden">`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
-      document.getElementById("purchase_record_number").removeAttribute("name");
-      document.getElementById("purchase_record_cvc").removeAttribute("name");
-      document.getElementById("purchase_record_exp_month").removeAttribute("name");
-      document.getElementById("purchase_record_exp_year").removeAttribute("name");
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
 
       document.getElementById("charge-form").submit();
     });
